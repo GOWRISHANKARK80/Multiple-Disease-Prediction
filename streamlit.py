79579321c4eb3ee47d71537d16f868e4ebed9b6b
Gowri Shankar K <carthishank80@gmail.com>
2025-03-18 21:59:14 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,23 +10,21 @@ def set_background_image_local(image_path):
     with open(image_path, "rb") as file:
         data = file.read()
     base64_image = base64.b64encode(data).decode("utf-8")
-    
     st.markdown(
         f"""
         <style>
         .stApp {{
-            background-image: url("data:image/jpeg;base64,{base64_image}");
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
+            background-image: url("data:image/png;base64,{base64_image}");
+            background-size: contain;
+            background-position: fit;
+            background-repeat: repeat;
             background-attachment: fixed;
         }}     
         </style>
         """,
         unsafe_allow_html=True
-    ) 
+    )
 
-# Call the function
 set_background_image_local(r"1.png")
 # Streamlit UI
 st.title("Disease Prediction")
